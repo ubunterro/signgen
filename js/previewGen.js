def443074f2fc3f1cda12ce14e53456703d9f60b
ubunterro <finko-ilya@yandex.ru>
2020-04-22 22:06:54 +0300

    исправил чето

--- js/previewGen.js
+++ js/previewGen.js
@@ -1,26 +1,5 @@
-/** @description Генерирует на главной странице превью адресной таблички из введённых данных
- */
-
-/*
-"fieldStreetType">
-
-<!--
-<p>&larr; Первый дом</p>
-<input type="text" id="startNumber">
-
-<p>&rarr;</p>
--->
-
-<p>Название на английском</p>
-<!--Наименование на английском-->
-<input type="text" id="fieldStreetNameEnglish">
-
-<p>Номер дома</p>
-<input type="text" id="fieldHouseNumber">
-*/
-
 $(document).ready(function(){
-    $("#buttonGenerateSign").click(function(){
+    /**$("#buttonGenerateSign").click(function(){
 
         var streetName = $("#fieldStreetName").val();
         var streetType = $("#fieldStreetType").val();
@@ -32,6 +11,6 @@ $(document).ready(function(){
         $("#signHouseNumber").text(houseNumber);
         $("#signStreetNameEnglish").text(streetNameEnglish);
     });
-
+    */
 });
 
